0a052be9daf1181a0ae475405d818988a10d9ecf
Valeria Muner <vmuner@kopiustech.com>
2023-12-16 07:54:17 -0300

    se modifico un archivo

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.static('public', {
 app.use(express.json());
 app.use(cookieParser());
 app.use("health", (req, res) => res.sendStatus(200));
-app.use('/', routes)
+app.use('/', () => routes);
 
 mongoose.connect(uri).
 then(() => {
